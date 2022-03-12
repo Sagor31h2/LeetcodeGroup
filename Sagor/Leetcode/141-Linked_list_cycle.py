0100d8c8b96e4013e8025ffa1fd91886202766e2
sagor31h2 <sagor31h2@gmail.com>
2022-03-13 00:19:19 +0530

    13 march 2022

---
+++ Sagor/Leetcode/141-Linked_list_cycle.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        li = []
+        temp = head
+        while temp:
+            if temp in li:
+                return True
+            li.append(temp)
+            temp = temp.next
+        return False
