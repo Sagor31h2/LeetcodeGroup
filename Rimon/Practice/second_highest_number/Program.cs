c2f5adfa9b4df26cf8090d305fd5423b3a0f72fa
mosfak <mosfak@pop-os.localdomain>
2022-04-07 05:49:03 +0600

    first csharp program

---
+++ Rimon/Practice/second_highest_number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace second_highest_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] l = {3,34,5,4,56,7,67,45,34,67,2,2,2};
+            var a = 0;
+            var b = 0;
+            foreach (int i in l){
+                if(i>a){
+                    b = a;
+                    a = i;
+                }
+                else
+                    if(i>b && i!=a)
+                        b = i; 
+            }
+            Console.Write("{0},{1}\n",a,b);
+        }
+    }
+}
